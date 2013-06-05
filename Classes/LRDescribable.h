a0db0ff541d0641759579a239114a4b3a821fab3
Robert Gilliam <robert@robertgilliam.org>
2013-06-05 10:38:35 -0700

    remove more UIKit dependencies

--- Classes/LRDescribable.h
+++ Classes/LRDescribable.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2010 LJR Software Limited. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+//#import <UIKit/UIKit.h>
 
 @class LRExpectationMessage;
 
--- Classes/LRMocky/LRExpectation.h
+++ Classes/LRMocky/LRExpectation.h
@@ -6,7 +6,7 @@
 //  Copyright 2010 LJR Software Limited. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+//#import <UIKit/UIKit.h>
 #import "LRExpectationAction.h"
 #import "LRDescribable.h"
 
